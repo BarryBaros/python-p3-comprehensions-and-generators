8644cc1ecf5a6a1ea55dabb8d0d9fa33657b3f76
Barasa <timbaros87@gmail.com>
2024-06-29 23:39:08 +0300

    Returns a list of all even numbers

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 == 0]
+    
+if __name__ == "__main__":
+     print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
     pass
