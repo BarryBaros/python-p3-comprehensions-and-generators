dba42f00f4aeb4ecae9d834f07d0577fc1e47847
Barasa <timbaros87@gmail.com>
2024-06-29 23:57:54 +0300

    Returns a list of sentence strings with exclamation marks at the end

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -7,5 +7,10 @@ if __name__ == "__main__":
      print(return_evens([0, 1, 3, 5, 7, 8, 9]))
 
 
+
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+
+if __name__ == "__main__":
+    sentence = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+    print(sentence)
